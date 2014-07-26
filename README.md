6433c080013312914998d913804d2febf1623f37
pokutuna <popopopopokutuna@gmail.com>
2014-07-26 15:25:31 +0900

    minil new JSON::Schema

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+JSON::Schema - It's new $module
+
+# SYNOPSIS
+
+    use JSON::Schema;
+
+# DESCRIPTION
+
+JSON::Schema is ...
+
+# LICENSE
+
+Copyright (C) pokutuna.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+pokutuna <popopopopokutuna@gmail.com>
---
+++ lib/JSON/Schema.pm
@@ -0,0 +1,39 @@
+package JSON::Schema;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+JSON::Schema - It's new $module
+
+=head1 SYNOPSIS
+
+    use JSON::Schema;
+
+=head1 DESCRIPTION
+
+JSON::Schema is ...
+
+=head1 LICENSE
+
+Copyright (C) pokutuna.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+pokutuna E<lt>popopopopokutuna@gmail.comE<gt>
+
+=cut
+
