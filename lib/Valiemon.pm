332e77135acdcf858eda18f7d318d6319d43378d
pokutuna <popopopopokutuna@gmail.com>
2016-04-19 17:00:42 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 12b1b08..66fe890 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl extension Valiemon
    
     {{$NEXT}}
    
    +0.04 2016-04-19T07:59:27Z
    +    - accept many attributes
    +      (astj)
    +    - test with JSON-Schema-Test-Suite
    +      (hitode909)
    +
     0.03 2015-08-17T01:27:42Z
    
         - fix accessor typo

--- lib/Valiemon.pm
+++ lib/Valiemon.pm
@@ -13,7 +13,7 @@ use Class::Accessor::Lite (
     ro => [qw(schema options pos schema_cache)],
 );
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 
 sub new {
     my ($class, $schema, $options) = @_;
