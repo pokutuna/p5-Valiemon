4fafb5ffe80b5853caeb6677266fdcd6d0576253
Asato Wakisaka <asato.wakisaka@gmail.com>
2016-04-19 15:28:18 +0900

    More simplyfy (suggestion by @pokutuna)

--- lib/Valiemon/Attributes/AnyOf.pm
+++ lib/Valiemon/Attributes/AnyOf.pm
@@ -19,28 +19,12 @@ sub is_valid {
         croak sprintf '`anyOf` must be an array and have at least one element at %s', $context->position
     }
 
-    my $idx = 0;
-    my $errors = [];
-    # Clone context to avoid influencing sub_validator's error to main context.
-    my $is_valid = any {
-        my $clone_context = Valiemon::Context->clone_from($context);
-        my ($is_partially_valid, $error) = $clone_context->in_attr($class, sub {
-            my $sub_v = $clone_context->sub_validator($_);
-            $clone_context->in(
-                $idx++, sub { $sub_v->validate($data, $clone_context); }
-            );
-        });
-        # If an error occured keep it so that main context will return it as main context's validation error.
-        if ($error) {
-            push @$errors, $error;
-        }
-        $is_partially_valid;
-    } @$anyOf;
-
-    unless ($is_valid) {
-        return (0, $errors->[0]);
-    }
-    return 1;
+    $context->in_attr($class, sub {
+        any {
+            my $clone_context = Valiemon::Context->clone_from($context);
+            $clone_context->sub_validator($_)->validate($data, $clone_context);
+        } @$anyOf;
+    });
 }
 
 1;
--- lib/Valiemon/Attributes/OneOf.pm
+++ lib/Valiemon/Attributes/OneOf.pm
@@ -19,32 +19,12 @@ sub is_valid {
         croak sprintf '`oneOf` must be an array and have at least one element at %s', $context->position
     }
 
-    my $idx = 0;
-    my $errors = [];
-    # Clone context to avoid influencing sub_validator's error to main context.
-    my $is_valid = one {
-        my $clone_context = Valiemon::Context->clone_from($context);
-        my ($is_partially_valid, $error) = $clone_context->in_attr($class, sub {
-            my $sub_v = $clone_context->sub_validator($_);
-            $clone_context->in(
-                $idx++, sub { $sub_v->validate($data, $clone_context); }
-            );
-        });
-        # If an error occured keep it so that main context will return it as main context's validation error.
-        if ($error) {
-            push @$errors, $error;
-        }
-        $is_partially_valid;
-    } @$oneOf;
-
-    unless ($is_valid) {
-        return (0, $errors->[0]) if scalar @$errors;
-
-        # If all of validation succeeds, there're no error to return.
-        # so run dummy validation to generate error.
-        return $context->in_attr($class, sub { return 0; });
-    }
-    return 1;
+    $context->in_attr($class, sub {
+        one {
+            my $clone_context = Valiemon::Context->clone_from($context);
+            $clone_context->sub_validator($_)->validate($data, $clone_context);
+        } @$oneOf;
+    });
 }
 
 1;
